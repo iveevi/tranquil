39143308150292a89c8ba9c747d3f23e8d1dd53b
Venkataram <vesion4690@gmail.com>
2022-11-04 15:41:02 -0700

    Fixing Camera view matrix

--- src/main.rs
+++ src/main.rs
@@ -1,166 +1,19 @@
 use std::fs;
-use std::ops;
 
 #[macro_use]
 extern crate glium;
 
-// Vector types
-#[derive(Copy, Clone)]
-struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
-}
+// Modules
+mod math;
+
+use math::*;
 
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 3],
 }
 
-// Math functions
-fn radians(degrees: f32) -> f32 {
-    degrees * std::f32::consts::PI / 180.0
-}
-
-fn length(v: Vec3) -> f32 {
-    (v.x * v.x + v.y * v.y + v.z * v.z).sqrt()
-}
-
-fn dot(v1: Vec3, v2: Vec3) -> f32 {
-    v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
-}
-
-fn normalize(v: Vec3) -> Vec3 {
-    let l = length(v);
-    Vec3 {
-        x: v.x / l,
-        y: v.y / l,
-        z: v.z / l
-    }
-}
-
-fn cross(a: Vec3, b: Vec3) -> Vec3 {
-    Vec3 {
-        x: a.y * b.z - a.z * b.y,
-        y: a.z * b.x - a.x * b.z,
-        z: a.x * b.y - a.y * b.x,
-    }
-}
-
-impl ops::Add for Vec3 {
-    type Output = Vec3;
-
-    fn add(self, other: Vec3) -> Vec3 {
-        Vec3 {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
-        }
-    }
-}
-
-impl ops::Sub for Vec3 {
-    type Output = Vec3;
-
-    fn sub(self, other: Vec3) -> Vec3 {
-        Vec3 {
-            x: self.x - other.x,
-            y: self.y - other.y,
-            z: self.z - other.z,
-        }
-    }
-}
-
 implement_vertex!(Vertex, position);
-
-// Matrix type
-#[derive(Copy, Clone, Debug)]
-struct Matrix {
-    data: [[f32; 4]; 4],
-}
-
-// Matrix functions
-impl Matrix {
-    // Get position and axes
-    fn position(&self) -> Vec3 {
-        Vec3 {
-            x: self.data[0][3],
-            y: self.data[1][3],
-            z: self.data[2][3],
-        }
-    }
-
-    fn forward(&self) -> Vec3 {
-        Vec3 {
-            x: self.data[0][2],
-            y: self.data[1][2],
-            z: self.data[2][2],
-        }
-    }
-
-    fn up(&self) -> Vec3 {
-        Vec3 {
-            x: self.data[0][1],
-            y: self.data[1][1],
-            z: self.data[2][1],
-        }
-    }
-
-    fn right(&self) -> Vec3 {
-        Vec3 {
-            x: self.data[0][0],
-            y: self.data[1][0],
-            z: self.data[2][0],
-        }
-    }
-
-    // Matrix generators
-    fn identity() -> Matrix {
-        Matrix {
-            data: [
-                [1.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0f32]
-            ]
-        }
-    }
-
-    // Assume fov to be in degrees
-    fn perspective(fov: f32, aspect: f32, znear: f32, zfar: f32) -> Matrix {
-        assert!(fov > 0.0 && fov < 180.0);
-        assert!(znear > 0.0);
-        assert!(zfar > znear);
-
-        let rfov = radians(fov);
-        let hfov = (rfov/2.0).tan();
-
-        Matrix {
-            data: [
-                [1.0/(aspect * hfov), 0.0, 0.0, 0.0],
-                [0.0, 1.0/hfov, 0.0, 0.0],
-                [0.0, 0.0, -(zfar + znear)/(zfar - znear), -1.0],
-                [0.0, 0.0, -(2.0 * zfar * znear)/(zfar - znear), 0.0]
-            ]
-        }
-    }
-
-    // Look at matrix
-    fn look_at(eye: Vec3, center: Vec3, up: Vec3) -> Matrix {
-        let f = normalize(center - eye);
-        let u = normalize(up);
-        let s = normalize(cross(f, u));
-
-        Matrix {
-            data: [
-                [s.x, s.y, s.z, -dot(s, eye)],
-                [u.x, u.y, u.z, -dot(u, eye)],
-                [-f.x, -f.y, -f.z, dot(f, eye)],
-                [0.0, 0.0, 0.0, 1.0]
-            ]
-        }
-    }
-}
 
 // Camera
 #[derive(Copy, Clone, Debug)]
@@ -192,9 +45,10 @@ impl Camera {
 
 fn main()
 {
-    let camera = Camera::new();
+    let mut camera = Camera::new();
     println!("Camera: {:?}", camera);
     println!("View: {:?}", camera.view());
+    println!("Perspective: {:?}", camera.perspective());
 
     // Read vertex and fragment shaders
     let vertex_shader_src = fs::read_to_string("src/base.vert")
@@ -202,4 +56,98 @@ fn main()
 
     let fragment_shader_src = fs::read_to_string("src/base.frag")
         .expect("Failed to read fragment shader");
+
+    // Setup Glium
+    use glium::{glutin, Surface};
+
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    // Testing mesh
+    let vertex1 = Vertex { position: [-0.5, -0.5, 5.0] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5, 5.0] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25, 5.0] };
+
+    let mut shape = vec![vertex1, vertex2, vertex3];
+
+    // Add offset triangles
+    for i in 0..10 {
+        let vertex1 = Vertex { position: [-0.5, -0.5, 5.0 + i as f32] };
+        let vertex2 = Vertex { position: [ 0.0,  0.5, 5.0 + i as f32] };
+        let vertex3 = Vertex { position: [ 0.5, -0.25, 5.0 + i as f32] };
+
+        shape.push(vertex1);
+        shape.push(vertex2);
+        shape.push(vertex3);
+    }
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    // Load shaders
+    let program = glium::Program::from_source(
+        &display, &vertex_shader_src,
+        &fragment_shader_src, None
+    ).unwrap();
+
+    let epoch = std::time::Instant::now();
+
+    event_loop.run(move |ev, _, control_flow| {
+        // Draw to screen
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        let uniforms = uniform! {
+            model: Matrix::identity().data,
+            view: camera.view().data,
+            projection: camera.perspective().data,
+        };
+
+        target.draw(
+            &vertex_buffer, &indices, &program,
+            &uniforms, &Default::default()
+        ).unwrap();
+
+        target.finish().unwrap();
+
+        // Move camera in a circle
+        let t = std::time::Instant::now();
+        let t = t.duration_since(epoch).as_secs_f32();
+        let x = t.cos() * 5.0;
+        let z = t.sin() * 5.0;
+
+        camera.transform.data[0][3] = x;
+        camera.transform.data[2][3] = z;
+
+        println!("\nt = {}, x = {}, z = {}", t, x, z);
+        println!("\tCamera: {:?}", camera.transform.position());
+
+        // Schedule a redraw
+        let next_frame_time = std::time::Instant::now()
+            + std::time::Duration::from_nanos(16_666_667);
+
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        // Manage events
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+
+                glutin::event::WindowEvent::KeyboardInput { input, .. } => {
+                    if let Some(glutin::event::VirtualKeyCode::Escape) = input.virtual_keycode {
+                        *control_flow = glutin::event_loop::ControlFlow::Exit;
+                        return;
+                    }
+                },
+
+                _ => return,
+            },
+            _ => (),
+        }
+    });
 }
---
+++ src/math.rs
@@ -0,0 +1,153 @@
+use std::ops;
+
+// Math functions
+fn radians(degrees: f32) -> f32 {
+    degrees * std::f32::consts::PI / 180.0
+}
+
+// Vector types and functions
+#[derive(Copy, Clone, Debug)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+pub fn length(v: Vec3) -> f32 {
+    (v.x * v.x + v.y * v.y + v.z * v.z).sqrt()
+}
+
+pub fn dot(v1: Vec3, v2: Vec3) -> f32 {
+    v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+}
+
+pub fn normalize(v: Vec3) -> Vec3 {
+    let l = length(v);
+    Vec3 {
+        x: v.x / l,
+        y: v.y / l,
+        z: v.z / l
+    }
+}
+
+pub fn cross(a: Vec3, b: Vec3) -> Vec3 {
+    Vec3 {
+        x: a.y * b.z - a.z * b.y,
+        y: a.z * b.x - a.x * b.z,
+        z: a.x * b.y - a.y * b.x,
+    }
+}
+
+// Matrix type
+#[derive(Copy, Clone, Debug)]
+pub struct Matrix {
+    pub data: [[f32; 4]; 4],
+}
+
+// Matrix functions
+impl Matrix {
+    // Get position and axes
+    pub fn position(&self) -> Vec3 {
+        Vec3 {
+            x: self.data[0][3],
+            y: self.data[1][3],
+            z: self.data[2][3],
+        }
+    }
+
+    pub fn forward(&self) -> Vec3 {
+        Vec3 {
+            x: self.data[0][2],
+            y: self.data[1][2],
+            z: self.data[2][2],
+        }
+    }
+
+    pub fn up(&self) -> Vec3 {
+        Vec3 {
+            x: self.data[0][1],
+            y: self.data[1][1],
+            z: self.data[2][1],
+        }
+    }
+
+    pub fn right(&self) -> Vec3 {
+        Vec3 {
+            x: self.data[0][0],
+            y: self.data[1][0],
+            z: self.data[2][0],
+        }
+    }
+
+    // Matrix generators
+    pub fn identity() -> Matrix {
+        Matrix {
+            data: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32]
+            ]
+        }
+    }
+
+    // Assume fov to be in degrees
+    pub fn perspective(fov: f32, aspect: f32, znear: f32, zfar: f32) -> Matrix {
+        assert!(fov > 0.0 && fov < 180.0);
+        assert!(znear > 0.0);
+        assert!(zfar > znear);
+
+        let rfov = radians(fov);
+        let f = 1.0/(rfov/2.0).tan();
+
+        Matrix {
+            data: [
+                [aspect/f, 0.0, 0.0, 0.0],
+                [0.0, f, 0.0, 0.0],
+                [0.0, 0.0, (zfar + znear)/(zfar - znear), 1.0],
+                [0.0, 0.0, -(2.0 * zfar * znear)/(zfar - znear), 0.0]
+            ]
+        }
+    }
+
+    // Look at matrix
+    pub fn look_at(eye: Vec3, center: Vec3, up: Vec3) -> Matrix {
+        let f = normalize(center - eye);
+        let u = normalize(up);
+        let s = normalize(cross(f, u));
+
+        Matrix {
+            data: [
+                [s.x, u.x, f.x, 0.0],
+                [s.y, u.y, f.y, 0.0],
+                [s.z, u.z, f.z, 0.0],
+                [-dot(s, eye), -dot(u, eye), -dot(f, eye), 1.0],
+            ]
+        }
+    }
+}
+
+// Arithmetic
+impl ops::Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
